6774a876cb49b14839c98964ffebf520692cd440
RcodePlay <ryancode.eh@gmail.com>
2024-05-21 20:37:24 +0200

    feat: :sparkles: Added all functions for event management
    Added all important functions for managing events in database through API. It wasn't tested, because node dosn't understand typescript even with ts-node. I'm gonna cry.

--- server/controllers/events.controllers.ts
+++ server/controllers/events.controllers.ts
@@ -6,7 +6,7 @@ import {
   deleteItem,
 } from '../data-models/events.models';
 
-export const getEvent = (req, res) => {
+export const getEvent = async (req, res) => {
   try {
     const resp = getItem(parseInt(req.params.id));
     res.status(200).json(resp);
@@ -15,7 +15,7 @@ export const getEvent = (req, res) => {
   }
 };
 
-export const listEvents = (req, res) => {
+export const listEvents = async (req, res) => {
   try {
     const resp = listItems();
     res.status(200).json(resp);
@@ -24,7 +24,7 @@ export const listEvents = (req, res) => {
   }
 };
 
-export const editEvent = (req, res) => {
+export const editEvent = async (req, res) => {
   try {
     const resp = editItem(parseInt(req.params.id), req.body);
     res.status(200).json(resp);
@@ -33,7 +33,7 @@ export const editEvent = (req, res) => {
   }
 };
 
-export const addEvent = (req, res) => {
+export const addEvent = async (req, res) => {
   try {
     const resp = addItem(req.body);
     res.status(200).json(resp);
@@ -42,7 +42,7 @@ export const addEvent = (req, res) => {
   }
 };
 
-export const deleteEvent = (req, res) => {
+export const deleteEvent = async (req, res) => {
   try {
     const resp = deleteItem(parseInt(req.params.id));
     res.status(200).json(resp);
--- server/data-models/events.models.ts
+++ server/data-models/events.models.ts
@@ -1,40 +1,97 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import Event from '../models/event';
 
 dotenv.config();
 
-const db = process.env.DB;
+const db = process.env.DB as string;
 mongoose.set('strictQuery', false);
 mongoose.connect(db);
 
-export const getItem = (id) => {
+export const getItem = async (id) => {
   try {
-  } catch (err) {}
+    const event = await Event.findById(id);
+    return event;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const listItems = () => {
   try {
+    const events = Event.find()
+      .sort({ num: -1 })
+      .exec((err, events) => {
+        if (err) {
+          console.log(err);
+        } else {
+          return events;
+        }
+      });
   } catch (err) {
     console.log(err);
   }
 };
 
-export const editItem = (id, data) => {
+export const editItem = async (id, data) => {
   try {
+    const { title, content, num } = data;
+    const updatedArticle = await Event.findByIdAndUpdate(id, {
+      title,
+      content,
+      num,
+    });
   } catch (err) {
     console.log(err);
   }
 };
 
-export const addItem = (data) => {
+export const addItem = async (data) => {
   try {
+    const { title, content } = data;
+    if (!title || !content) {
+      console.log('Title and contet are required fields!');
+    }
+
+    Event.findOne()
+      .sort({ num: -1 })
+      .exec(async (err, latestEvent) => {
+        if (err) {
+          console.log(err);
+        } else {
+          let latestNum = latestEvent.num;
+          console.log(latestEvent.num);
+          let newNum = latestNum + 1;
+          console.log(newNum);
+
+          const event = new Event({
+            title: data.title,
+            content: data.content,
+            num: newNum,
+          });
+          const newEvent = await event.save();
+          console.log(newEvent);
+          return newEvent;
+        }
+      });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const deleteItem = (id) => {
+export const deleteItem = async (id) => {
   try {
+    if (!id) {
+      console.log('Event ID not recieved');
+    }
+
+    const deletedEvent = await Event.findByIdAndDelete(id);
+
+    if (!deletedEvent) {
+      console.log('Event not deleted');
+    }
+
+    return 'event removed successfully';
   } catch (error) {
     console.log(error);
   }
